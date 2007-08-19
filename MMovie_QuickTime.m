cfce95177ca818f109750e4375845e090c9a4397
cocoable <cocoable@9988c26d-9134-0410-b5bb-5778289bb252>
2007-08-19 08:34:46 +0000

    GPL related elements(copyright, copying permission) added for all files. README file added.
    git-svn-id: http://movist.googlecode.com/svn/trunk@20 9988c26d-9134-0410-b5bb-5778289bb252

--- MMovie_QuickTime.m
+++ MMovie_QuickTime.m
@@ -1,8 +1,23 @@
 //
 //  Movist
 //
-//  Created by dckim <cocoable@gmail.com>
-//  Copyright 2006 cocoable. All rights reserved.
+//  Copyright 2006, 2007 Yong-Hoe Kim. All rights reserved.
+//      Yong-Hoe Kim  <cocoable@gmail.com>
+//
+//  This file is part of Movist.
+//
+//  Movist is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Movist is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import "MMovie_QuickTime.h"
