e76be6b40ca62bf00e70ba596ac4adc9c4f053ef
moosoy <moosoy@9988c26d-9134-0410-b5bb-5778289bb252>
2009-04-05 03:39:23 +0000

    use ffmpeg v0.5
    git-svn-id: http://movist.googlecode.com/svn/trunk@254 9988c26d-9134-0410-b5bb-5778289bb252

--- contrib/src/build_ffmpeg.sh
+++ contrib/src/build_ffmpeg.sh
@@ -9,6 +9,7 @@ EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-v
 CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
 LDFLAGS="-L$PREFIX/lib"
 
+FFMPEG_VERSION=0.5
 FFMPEG_REVISION=11914
 FFMPEG_SWSCALE_REVISION=25987
 
@@ -17,17 +18,26 @@ PATH="$PREFIX/bin:$PATH"
 FFMPEG_CONF_COMMON=
 FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-vhook --disable-ffserver --disable-ffmpeg --disable-ffplay"
 FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-encoders --disable-muxers --disable-network"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-gpl --enable-pp --enable-swscaler"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-gpl --enable-postproc --enable-swscale"
 FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-pthreads"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-libfaad --enable-liba52"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-libfaad"
 FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-ffplay"
 
 ########## SOURCE ##########
 
 rm -rf ffmpeg
-svn co -r $FFMPEG_REVISION svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg
-(cd ffmpeg/libswscale && svn up -r $FFMPEG_SWSCALE_REVISION)
-(cd ffmpeg&& patch -p0 < ../Patches/ffmpeg-macosx-intel-mmx.patch)
+
+#ffmpeg 0.5
+if [ ! -e "ffmpeg-$FFMPEG_VERSION.tar.bz2" ]; then
+	curl -L -O http://www.ffmpeg.org/releases/ffmpeg-$FFMPEG_VERSION.tar.bz2
+fi
+tar xvfj ffmpeg-$FFMPEG_VERSION.tar.bz2
+mv ffmpeg-$FFMPEG_VERSION ffmpeg
+
+#svn co -r $FFMPEG_REVISION svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg
+#(cd ffmpeg/libswscale && svn up -r $FFMPEG_SWSCALE_REVISION)
+#(cd ffmpeg&& patch -p0 < ../Patches/ffmpeg-macosx-intel-mmx.patch)
+
 
 ########## INTEL ###########
 
@@ -51,7 +61,7 @@ EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-v
 CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
 LDFLAGS="-L$PREFIX/lib"
 
-FFMPEG_CONF_PPC=" --cross-compile --arch=ppc"
+FFMPEG_CONF_PPC="--enable-cross-compile --arch=ppc"
 FFMPEG_CFLAGS_PPC="-arch ppc -mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple"
 FFMPEG_LDFLAGS_PPC="-arch ppc"
 
--- make_univlib.sh
+++ make_univlib.sh
@@ -6,5 +6,9 @@ cd "$CONTRIB_PATH"
 mkdir -p lib
 cd i386/lib
 for aa in `ls *.a` ; do
-	lipo -create -arch i386 ${aa} -arch ppc ../../ppc/lib/${aa} -output ../../lib/${aa}
+	if [ -e "../../ppc/lib/$aa" ]; then
+		lipo -create -arch i386 ${aa} -arch ppc ../../ppc/lib/${aa} -output ../../lib/${aa}
+	else
+		cp $aa ../../lib/$aa
+	fi
 done
