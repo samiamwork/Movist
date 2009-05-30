c1b057d6984611664a4016444ab682afe3e59ca6
moosoy <moosoy@9988c26d-9134-0410-b5bb-5778289bb252>
2009-05-30 00:32:45 +0000

    use old ffmpeg in ppc
    git-svn-id: http://movist.googlecode.com/svn/trunk@267 9988c26d-9134-0410-b5bb-5778289bb252

--- contrib/src/build_ffmpeg.sh
+++ contrib/src/build_ffmpeg.sh
@@ -10,8 +10,10 @@ CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
 LDFLAGS="-L$PREFIX/lib"
 
 FFMPEG_VERSION=0.5
-FFMPEG_REVISION=18822
-FFMPEG_SWSCALE_REVISION=29308
+FFMPEG_REVISION=18971
+FFMPEG_SWSCALE_REVISION=29320
+FFMPEG_REVISION_PPC=11914
+FFMPEG_SWSCALE_REVISION_PPC=25987
 
 PATH="$PREFIX/bin:$PATH"
 
@@ -34,6 +36,8 @@ rm -rf ffmpeg
 #tar xvfj ffmpeg-$FFMPEG_VERSION.tar.bz2
 #mv ffmpeg-$FFMPEG_VERSION ffmpeg
 
+#svn co svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg
+
 svn co -r $FFMPEG_REVISION svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg
 (cd ffmpeg/libswscale && svn up -r $FFMPEG_SWSCALE_REVISION)
 #(cd ffmpeg&& patch -p0 < ../Patches/ffmpeg-macosx-intel-mmx.patch)
@@ -55,13 +59,23 @@ make clean && make && make install-libs && make install-headers)
 
 ##########  PPC  ###########
 
+svn co -r $FFMPEG_REVISION_PPC svn://svn.mplayerhq.hu/ffmpeg/trunk ffmpeg_ppc
+(cd ffmpeg_ppc/libswscale && svn up -r $FFMPEG_SWSCALE_REVISION_PPC)
+
 #PREFIX="/Users/moosoy/devel/movist_org/contrib/ppc"
 PREFIX="$(cd ..;pwd)/ppc"
 EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
 CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
 LDFLAGS="-L$PREFIX/lib"
 
-FFMPEG_CONF_PPC="--enable-cross-compile --arch=ppc"
+FFMPEG_CONF_COMMON=
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-ffserver --disable-ffmpeg --disable-ffplay"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-encoders --disable-muxers --disable-network"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-gpl --enable-pp"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-pthreads"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-libfaad"
+
+FFMPEG_CONF_PPC="--cross-compile --arch=ppc"
 FFMPEG_CFLAGS_PPC="-arch ppc -mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple"
 FFMPEG_LDFLAGS_PPC="-arch ppc"
 
@@ -69,6 +83,6 @@ FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_PPC"
 FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_PPC"
 FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_PPC"
 
-(cd ffmpeg && \
+(cd ffmpeg_ppc && \
 ./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
 make clean && make && make install-libs && make install-headers)
