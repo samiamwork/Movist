6eaf9248f86014951b7b2c7b4b4adddd58fc164c
moosoy <moosoy@9988c26d-9134-0410-b5bb-5778289bb252>
2009-11-29 09:13:58 +0000

    add build script ffmpeg-mt
    git-svn-id: http://movist.googlecode.com/svn/trunk@310 9988c26d-9134-0410-b5bb-5778289bb252

---
+++ contrib/src/build_ffmpeg_mt.sh
@@ -0,0 +1,62 @@
+#/bin/sh
+set -e 
+set -v
+
+SDK_TARGET="10.4"
+MACOSX_SDK="/Developer/SDKs/MacOSX10.4u.sdk"
+
+FFMPEG_CONF_COMMON=
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-ffserver --disable-ffmpeg --disable-ffplay"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-encoders --disable-muxers --disable-network"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-debug"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-gpl --enable-postproc"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-libfaad"
+FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-pthreads"
+
+########## SOURCE ##########
+
+if [ -d "ffmpeg-mt" ]; then 
+	git pull
+else
+	git clone git://gitorious.org/~astrange/ffmpeg/ffmpeg-mt.git
+	git clone git://git.ffmpeg.org/libswscale/ ffmpeg-mt/libswscale
+fi
+
+########## INTEL ###########
+
+PREFIX="$(cd ..;pwd)/i386"
+EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=${SDK_TARGET} -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
+CFLAGS="-I${MACOSX_SDK}/usr/include -I$PREFIX/include $EXTRA_CFLAGS"
+LDFLAGS="-L$PREFIX/lib"
+PATH="$PREFIX/bin:$PATH"
+
+FFMPEG_CONF_INTEL="--cpu=pentium-m"
+FFMPEG_CFLAGS_INTEL="-mtune=nocona -fstrict-aliasing -frerun-cse-after-loop -fweb -falign-loops=16"
+FFMPEG_LDFLAGS_INTEL="-arch i386"
+
+FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_INTEL"
+FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_INTEL"
+FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_INTEL"
+
+(cd ffmpeg-mt && \
+./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
+make clean && make && make install-libs && make install-headers)
+
+##########  PPC  ###########
+
+PREFIX="$(cd ..;pwd)/ppc"
+EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
+CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
+LDFLAGS="-L$PREFIX/lib"
+
+FFMPEG_CONF_PPC="--enable-cross-compile --arch=ppc"
+FFMPEG_CFLAGS_PPC="-arch ppc -mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple"
+FFMPEG_LDFLAGS_PPC="-arch ppc"
+
+FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_PPC"
+FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_PPC"
+FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_PPC"
+
+(cd ffmpeg-mt && \
+./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
+make clean && make && make install-libs && make install-headers)
