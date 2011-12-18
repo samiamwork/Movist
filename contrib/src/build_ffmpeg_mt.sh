cadcb8ece6dcde5c5e6d12fadfcea6a10d0a41ce
Nur Monson <samiamwork@gmail.com>
2011-12-18 12:10:36 -0800

    Switch to using libav mainline and using a submodule
    - Now people who checkout the code should be able to get the
      same build results as me instead of the crapshoot it was
      before. Hooray!
    - I don't see any good reason not to be tracking libav mainline.
      AStrange's stuff is nice but it's not updated often and I don't
      know what he does and doesn't have from mainline.
    - I had to hack the contrib/bootstrap script so it would build
      on 10.7 so for now you can't build unless you're on 10.7.
      I'll try to fix this (and clean up the bootstrap script) but
      right now I'm interested in just getting things building again.
    - Update Xcode project for Xcode 4.2
    - For some reason it wanted to build 64-bit even though it used
      to be set to build 32-bit only (there's still a bit of QTKit
      API that's 32-bit only).

--- contrib/src/build_ffmpeg_mt.sh
+++ contrib/src/build_ffmpeg_mt.sh
@@ -2,89 +2,57 @@
 set -e 
 set -v
 
-SDK_TARGET="10.6"
-MACOSX_SDK="/Developer/SDKs/MacOSX10.6.sdk"
+THESDK="/Developer/SDKs/MacOSX10.6.sdk"
+ORIGINAL_PATH="$PATH"
 
-FFMPEG_CONF_COMMON=
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-ffserver --disable-ffmpeg --disable-ffplay"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-encoders --disable-muxers --disable-network"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-ffprobe --disable-avdevice"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-debug --disable-doc"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-gpl --enable-postproc"
-#FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-libfaad"
-#FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-hardcoded-tables"
-#FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-runtime-cpudetect"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --disable-swscale"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --enable-pthreads"
-FFMPEG_CONF_COMMON="$FFMPEG_CONF_COMMON --cc=gcc-4.2"
+build_libav()
+{
+(cd libav && \
+./configure \
+--arch=$THEARC \
+--cpu=$THECPU \
+--cc=gcc-4.2 \
+--enable-decoders \
+--disable-vda \
+--disable-encoders \
+--enable-demuxers \
+--disable-muxers \
+--enable-parsers \
+--disable-avdevice \
+--enable-postproc \
+--disable-network \
+--enable-pthreads \
+--enable-gpl \
+--disable-avconv \
+--disable-ffmpeg \
+--disable-avprobe \
+--disable-avserver \
+--disable-avplay \
+--disable-swscale \
+--extra-ldflags="-L$PREFIX/lib -arch $THEARC -isystem $THESDK -mmacosx-version-min=10.6 -Wl,-syslibroot,$THESDK " \
+--extra-cflags="-isystem $PREFIX/include -arch $THEARC -isystem $THESDK -mmacosx-version-min=10.6 -Wno-deprecated-declarations $THEOPT " \
+--enable-protocol=file \
+--prefix=$PREFIX \
+&& make clean && make && make install-libs && make install-headers)
+}
 
-########## SOURCE ##########
-
-if [ -d "ffmpeg-mt" ]; then 
-	echo "ffmpeg-mt"
-	#(cd ffmpeg-mt && git pull)
-else
-	git clone git://gitorious.org/~astrange/ffmpeg/ffmpeg-mt.git
-	#git clone git://git.ffmpeg.org/libswscale/ ffmpeg-mt/libswscale
-	(cd ffmpeg-mt && patch -p1 < ../Patches/ffmpegmt-disablelibswscale-disablepic.patch)
-fi
-
-########## INTEL ###########
+########## INTEL i386 ###########
 
 PREFIX="$(cd ..;pwd)/i386"
-EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=${SDK_TARGET} -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
-CFLAGS="-I${MACOSX_SDK}/usr/include -I$PREFIX/include $EXTRA_CFLAGS"
-LDFLAGS="-L$PREFIX/lib -isysroot ${MACOSX_SDK} -mmacosx-version-min=10.6"
-PATH="$PREFIX/bin:$PATH"
+PATH="$PREFIX/bin:$ORIGINAL_PATH"
+THEARC="i386"
+THECPU="pentium-m"
+THEOPT=""
 
-FFMPEG_CONF_INTEL="--cpu=pentium-m"
-FFMPEG_CFLAGS_INTEL="-arch i386 -mtune=nocona -fstrict-aliasing -frerun-cse-after-loop -fweb -falign-loops=16"
-FFMPEG_LDFLAGS_INTEL="-arch i386"
-
-FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_INTEL"
-FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_INTEL"
-FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_INTEL"
-
-echo ./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS"
-(cd ffmpeg-mt && \
-./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
-make clean && make && make install-libs && make install-headers)
+build_libav
 
 ########## INTEL x86_64 ###########
 
 PREFIX="$(cd ..;pwd)/x86_64"
-EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=${SDK_TARGET} -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
-CFLAGS="-I${MACOSX_SDK}/usr/include -I$PREFIX/include $EXTRA_CFLAGS"
-LDFLAGS="-L$PREFIX/lib -isysroot ${MACOSX_SDK} -mmacosx-version-min=10.6"
-PATH="$PREFIX/bin:$PATH"
+PATH="$PREFIX/bin:$ORIGINAL_PATH"
+THEARC="x86_64"
+THECPU="core2"
+THEOPT="-mtune=core2"
 
-FFMPEG_CONF_INTEL="--cpu=core2"
-FFMPEG_CFLAGS_INTEL="-arch x86_64 -mtune=nocona -fstrict-aliasing -frerun-cse-after-loop -fweb -falign-loops=16"
-FFMPEG_LDFLAGS_INTEL="-arch x86_64"
+build_libav
 
-FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_INTEL"
-FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_INTEL"
-FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_INTEL"
-
-(cd ffmpeg-mt && \
-./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
-make clean && make && make install-libs && make install-headers)
-
-##########  PPC  ###########
-
-#PREFIX="$(cd ..;pwd)/ppc"
-#EXTRA_CFLAGS="-isysroot ${MACOSX_SDK} -DMACOSX_DEPLOYMENT_TARGET=10.4 -mmacosx-version-min=${SDK_TARGET} -isystem $PREFIX/include"
-#CFLAGS="-I$PREFIX/include $EXTRA_CFLAGS"
-#LDFLAGS="-L$PREFIX/lib -isysroot ${MACOSX_SDK} -mmacosx-version-min=10.4"
-
-#FFMPEG_CONF_PPC="--enable-cross-compile --arch=ppc --target-os=darwin"
-#FFMPEG_CFLAGS_PPC="-arch ppc -mcpu=G3 -mtune=G5 -fstrict-aliasing -funroll-loops -falign-loops=16 -mmultiple"
-#FFMPEG_LDFLAGS_PPC="-arch ppc"
-
-#FFMPEG_CONF="$FFMPEG_CONF_COMMON $FFMPEG_CONF_PPC"
-#FFMPEG_CFLAGS="$CFLAGS $FFMPEG_CFLAGS_PPC"
-#FFMPEG_LDFLAGS="$LDFLAGS $FFMPEG_LDFLAGS_PPC"
-
-#(cd ffmpeg-mt && \
-#./configure $FFMPEG_CONF --prefix=$PREFIX --extra-cflags="$FFMPEG_CFLAGS" --extra-ldflags="$FFMPEG_LDFLAGS" && \
-#make clean && make && make install-libs && make install-headers)
